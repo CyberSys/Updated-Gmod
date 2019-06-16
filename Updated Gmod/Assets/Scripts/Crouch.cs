75944665bbe509944c1498a98b287a2b422cfb49
Mitch-War <mitchwartheboss@gmail.com>
2019-06-16 20:49:17 +0200

    Initial Upload
    Basiacally just the base.

---
+++ Updated Gmod/Assets/Scripts/Crouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public CharacterController characterController;
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            characterController.height = 1f;
+        }
+        else
+        {
+            characterController.height = 2f;
+        }
+    }
+}
---
+++ Updated Gmod/Assets/Scripts/playerMove.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    //Calls functions and basic character movements.
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed * Time.deltaTime;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed * Time.deltaTime;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+
+        Running();
+
+        Crouching();
+    }
+
+    //Checks for Space Bar press.
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    // Calculates jumping if available.
+    private IEnumerator JumpEvent()
+    {
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpforce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpforce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        isJumping = false;
+    }
+
+    //This checks if Player is walking and is not in the air and applies more speed.
+    private void Running()
+    {
+        if (movementSpeed == 200f && !isJumping == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                movementSpeed = 600f;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                movementSpeed = 200f;
+            }
+        }
+    }
+
+    //This checks if Player speed is >= 200 and is not jumping then slows the Player accordingly.
+    private void Crouching()
+    {
+        if (movementSpeed >= 200f && !isJumping == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                movementSpeed = 50f;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                movementSpeed = 200f;
+            }
+        }
+    }
+}
